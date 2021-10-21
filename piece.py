d585c3a27ff1972de19ccce676d6fe639795ad60
Robin <robinwootton@gmail.com>
2021-10-21 14:20:27 +0000

    Makes us of Piece to string function

--- piece.py
+++ piece.py
@@ -13,4 +13,6 @@ class Piece:
             coord.y = coord.y * -1
 
     def __str__(self):
-        return "piece " + self.name + ' x: ' + str(self.focus.x) + ' y: ' + str(self.focus.y)
+        # return "piece " + self.name + ' x: ' + str(self.focus.x) + ' y: ' + str(self.focus.y)
+
+        return "Piece " + self.name + " position is  " + str(self.focus)
